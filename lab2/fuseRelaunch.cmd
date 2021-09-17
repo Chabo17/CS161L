60308ec31b8041bf7ce1432edab344a92b0e27a5
Chandler Bottomley <bottomleychad@gmail.com>
2021-09-17 09:16:51 -0700

    Add files via upload

---
+++ lab2/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "/home/ise/CS161L/lab2/fixedFloatConversion_tb_isim_beh.exe" -prj "/home/ise/CS161L/lab2/fixedFloatConversion_tb_beh.prj" "work.fixedFloatConversion_tb" "work.glbl" 
