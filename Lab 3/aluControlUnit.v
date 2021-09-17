1506f46acb3249c0fc243e4947d8cf61ed78d896
Chandler Bottomley <bottomleychad@gmail.com>
2021-09-17 09:28:09 -0700

    Add files via upload

---
+++ Lab 3/aluControlUnit.v
@@ -0,0 +1,48 @@
+//=========================================================================
+// Name & Email must be EXACTLY as in Gradescope roster!
+// Name: Chandler Bottomley Alex Nguyen 
+// Email: cbott001@ucr.edu anguy258@ucr.eduu
+// 
+// Assignment name: 
+// Lab section: 
+// TA: 
+// 
+// I hereby certify that I have not received assistance on this assignment,
+// or used code, from ANY outside source other than the instruction team
+// (apart from what was provided in the starter file).
+//
+//=========================================================================
+
+module aluControlUnit (
+    input  wire [1:0] alu_op, 
+    input  wire [5:0] instruction_5_0, 
+    output wire [3:0] alu_out
+    );
+	 
+reg t_alu_out = 4'b0111;
+
+always @(*)begin
+	case(alu_op) 
+	
+	2'b00: begin t_alu_out = 4'b0010; end//LW + SW
+	2'b01: begin t_alu_out = 4'b0110; end// Branch equal
+	2'b11: begin t_alu_out = 4'b0110; end// Branch equal
+	
+	2'b10: 
+		case(instruction_5_0) 
+			6'b100000 : begin  t_alu_out = 4'b0010; end//add
+			6'b100001 : begin t_alu_out = 4'b0010; end//addu
+			6'b100010 : begin t_alu_out = 4'b0110; end//sub
+			6'b100100 : begin t_alu_out = 4'b0000; end//AND
+			6'b100101 : begin t_alu_out = 4'b0001; end//OR
+			6'b100111 : begin t_alu_out = 4'b1100; end//NOR
+			6'b101010 : begin t_alu_out = 4'b0111; end//set on less than
+			
+		endcase
+	endcase
+	
+end 
+	
+assign alu_out = t_alu_out;
+
+endmodule
---
+++ Lab 3/controlUnit.v
@@ -0,0 +1,96 @@
+//=========================================================================
+// Name & Email must be EXACTLY as in Gradescope roster!
+// Name: Chandler Bottomley Alex Nguyen 
+// Email: cbott001@ucr.edu anguy258@ucr.eduu
+// 
+// Assignment name: 
+// Lab section: 
+// TA: 
+// 
+// I hereby certify that I have not received assistance on this assignment,
+// or used code, from ANY outside source other than the instruction team
+// (apart from what was provided in the starter file).
+//
+//=========================================================================
+
+module controlUnit  (
+    input wire [5:0]  instr_op , 
+    output reg reg_dst      ,   
+    output reg  branch    ,     
+    output reg  mem_read ,  
+    output reg  mem_to_reg  ,
+    output reg [1:0]  alu_op  ,        
+    output reg  mem_write  , 
+    output reg  alu_src     ,    
+    output reg  reg_write   
+    );
+
+// ------------------------------
+// Insert your solution below
+// ------------------------------ 
+
+always@(*) begin
+
+case(instr_op)
+	6'b000000://Rformat
+	begin
+		reg_dst = 1'b1;
+		alu_src = 1'b0;
+		mem_to_reg = 1'b0;
+		reg_write = 1'b1;
+		mem_read = 1'b0;
+		mem_write = 1'b0;
+		branch = 1'b0;
+		alu_op = 2'b10;
+	end
+		
+	6'b100011://lw
+	begin
+		reg_dst = 1'b0;
+		alu_src = 1'b1;
+		mem_to_reg = 1'b1;
+		reg_write = 1'b1;
+		mem_read = 1'b1;
+		mem_write = 1'b0;
+		branch = 1'b0;
+		alu_op = 2'b00;
+	end
+	
+	6'b101011://sw
+	begin
+		//reg_dist = 1'b0;
+		alu_src = 1'b1;
+		//mem_to_reg = 1'b0;
+		reg_write = 1'b0;
+		mem_read = 1'b0;
+		mem_write = 1'b1;
+		branch = 1'b0;
+		alu_op = 2'b00;
+	end
+	
+	6'b000100://beq
+	begin
+		//reg_dist = 1'b1;
+		alu_src = 1'b0;
+		//mem_to_reg = 1'b0;
+		reg_write = 1'b0;
+		mem_read = 1'b0;
+		mem_write = 1'b0;
+		branch = 1'b1;
+		alu_op = 2'b01;
+	end
+	
+	6'b001000://addi
+	begin
+		reg_dst = 1'b1;
+		alu_src = 1'b1;
+		mem_to_reg = 1'b0;
+		reg_write = 1'b1;
+		mem_read = 1'b0;
+		mem_write = 1'b0;
+		branch = 1'b0;
+		alu_op = 2'b10;
+	end
+	endcase
+	end
+endmodule
